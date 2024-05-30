9730abb0e515910f688ac9c1786c0536e2c5589f
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-05-29 23:11:35 -0300

    Feature - Sidebar finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,26 @@
+import { PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://images.unsplash.com/photo-1526040652367-ac003a0475fe?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1526040652367-ac003a0475fe?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            />
+
             <div className={styles.profile}>
-                <strong>
-                    Higor Stefany 
-                </strong>
-                <span>
-                    Web Developer
-                </span>
+                <img className={styles.avatar} src="https://github.com/higornobrega.png"/>
+
+                <strong>Higor Stefany</strong>
+                <span>Web Developer</span>
 
             </div>
             <footer>
                 <a href="">
+                    <PencilLine
+                        size={20}
+                    />
                     Editar seu perfil
                 </a>
             </footer>
