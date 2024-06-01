9f178b6e3952058fb95852f9be1465aa724bfe45
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-05-31 23:03:12 -0300

    Feature - Added component Post in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Post } from "./Post.jsx";
+import { Post } from "./components/Post.jsx";
 import { Header } from "./components/Header.jsx";
 import { Sidebar } from "./components/Sidebar.jsx";
 
