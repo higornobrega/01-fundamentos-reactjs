0487fd2a995a2c114c66098e53c81b253a325176
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-06-19 23:26:13 -0300

    Feature - Turning Avatar into a Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,26 @@
-import { PencilLine} from 'phosphor-react'
-import styles from './Sidebar.module.css'
+import { PencilLine } from "phosphor-react";
+import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            <img
-                className={styles.cover}
-                src="https://images.unsplash.com/photo-1526040652367-ac003a0475fe?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            />
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1526040652367-ac003a0475fe?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
 
-            <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/higornobrega.png"/>
-
-                <strong>Higor Stefany</strong>
-                <span>Web Developer</span>
-
-            </div>
-            <footer>
-                <a href="">
-                    <PencilLine
-                        size={20}
-                    />
-                    Editar seu perfil
-                </a>
-            </footer>
-        </aside>
-    )
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/higornobrega.png" />
+        <strong>Higor Stefany</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
